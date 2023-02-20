9f1c489399a2c2de7b0d9fa6d0f4a81a7752c870
cbmosley <cbmosley@ncsu.edu>
2023-02-20 15:21:06 -0500

    Completed intro to sequences

---
+++ working_with_sequences/README.md
@@ -0,0 +1,5 @@
+# General overview of what Biopython provides
+
+Biopython is a set of libraries to provide the ability to deal with “things” of interest to biologists working on the computer.
+
+##
--- working_with_sequences/sequences.py
+++ working_with_sequences/sequences.py
@@ -1,7 +1,15 @@
 from Bio.Seq import Seq
 
 # assigning value to my sequence of a genetic strand
+# Seq object differs from the Python string in the methods it supports
+# 5 prime 3 prime
 my_seq = Seq("AGTACACTGGT")
+
 print(my_seq)
+
 # printing my sequence complement strand
+# complement strand 3 prime to 5 prime
 print(my_seq.complement())
+
+# complement strand 5 prime to 3 prime
+print(my_seq.reverse_complement())
